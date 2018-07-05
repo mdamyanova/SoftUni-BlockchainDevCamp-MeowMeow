90d25d6fb9d16dd4638f0369d66a1378b792fcab
mdamyanova <mdamyanova181@gmail.com>
2018-07-05 19:28:26 +0300

    add gitignore and Java project

---
+++ MeowMeow/src/main/java/com/meowmeow/MeowMeowApplication.java
@@ -0,0 +1,12 @@
+package com.meowmeow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowMeowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeowMeowApplication.class, args);
+    }
+}
